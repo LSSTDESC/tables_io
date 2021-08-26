22a2f82a8cccf7f4c133bb70a4dc5e82ceeaef77
Eric Charles <echarles@gmail.com>
2021-08-26 14:49:10 -0700

    Fix docs/conf.py for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../tables_io'))
 try:
     import tables_io
 except:
-    print(sys.path)
+    pass
 
 # -- Project information -----------------------------------------------------
 
