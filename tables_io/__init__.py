f25d5cc85104f5bda50c08f0714c4bd753f9b068
Eric Charles <echarles@gmail.com>
2021-08-31 16:37:47 -0700

    protected against non-setup version

--- tables_io/__init__.py
+++ tables_io/__init__.py
@@ -1,6 +1,9 @@
 """tables_io is a library of functions for input, output and conversion of tabular data formats"""
 
-from ._version import version
+try:
+    from ._version import version
+except:
+    version = "unknown"
 
 from .lazy_modules import *
 
