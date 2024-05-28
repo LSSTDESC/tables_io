8ab7b5b4502efc9a2b6c6bba3c20dd8e5b60f022
Eric Charles <echarles@gmail.com>
2024-05-28 10:06:36 -0700

    switch to using cli

--- src/tables_io/cli.py
+++ src/tables_io/cli.py
@@ -1,35 +1,75 @@
 """cli for tables_io.convert"""
 
-import argparse  #pragma: no cover
+from functools import partial  #pragma: no cover
+from typing import Any, Type, TypeVar  #pragma: no cover
 
-import tables_io  #pragma: no cover
+import click  #pragma: no cover
 
-
-def get_args():  #pragma: no cover
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "input",
-        type=str,
-        help=f"input filename; suffix should be one of {list(tables_io.types.FILE_FORMAT_SUFFIXS.keys())}",
-    )
-    parser.add_argument(
-        "output",
-        type=str,
-        help=f"output filename; suffix should be one of {list(tables_io.types.FILE_FORMAT_SUFFIXS.keys())}",
-    )
-    return parser.parse_args()
+from . import types
+import tables_io
 
 
-def main():  #pragma: no cover
-    args = get_args()
+class PartialOption:
+    """Wraps click.option with partial arguments for convenient reuse"""
 
-    input_fname = args.input
-    output_fname = args.output
+    def __init__(self, *param_decls: Any, **kwargs: Any) -> None:
+        self._partial = partial(
+            click.option, *param_decls, cls=partial(click.Option), **kwargs
+        )
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:  # pragma: no cover
+        return self._partial(*args, **kwargs)
+
+
+class PartialArgument:
+    """Wraps click.argument with partial arguments for convenient reuse"""
+
+    def __init__(self, *param_decls: Any, **kwargs: Any) -> None:
+        self._partial = partial(
+            click.argument, *param_decls, cls=partial(click.Argument), **kwargs
+        )
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:  # pragma: no cover
+        return self._partial(*args, **kwargs)
+
+
+input = PartialOption(
+    "--input",
+    type=click.Path(),
+    help=f"input filename; suffix should be one of {list(types.FILE_FORMAT_SUFFIXS.keys())}",
+)
+
+output = PartialOption(
+    "--output",
+    type=click.Path(),
+    help=f"output filename; suffix should be one of {list(types.FILE_FORMAT_SUFFIXS.keys())}",
+)
+
+input_args = PartialArgument(
+    "input_args",
+    nargs=-1,
+)
+
+
+@click.group()
+@click.version_option(tables_io._version)
+def cli() -> None:
+    """tables_io utility scripts"""
+
+
+@cli.command()
+@input()
+@output()
+def convert(input, output):
+    """Convet a file with tabular data from one format to another"""
+
+    input_fname = input
+    output_fname = output
     output_format = output_fname.split(".")[-1]
 
     print(f"Converting {input_fname} to {output_fname}")
 
-    suffixes = tables_io.types.FILE_FORMAT_SUFFIXS
+    suffixes = types.FILE_FORMAT_SUFFIXS
     suffix = suffixes[output_format]
 
     t_in = tables_io.read(input_fname)
@@ -38,4 +78,15 @@ def main():  #pragma: no cover
 
     print(f"Done converting file")
 
+    return 0
+
+
+@cli.command()
+@output()
+@input_args()
+def make_index(output, input_args):
+    """Make an index file from a list of files"""
+
+    tables_io.createIndexFile(output, input_args)
+    
     return 0
