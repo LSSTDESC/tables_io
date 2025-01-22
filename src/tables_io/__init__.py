4863f35b990775105f63e6da77fc106846943c16
Mubdi Rahman <mubdi@sidratresearch.com>
2025-01-22 12:29:10 -0500

    Rearranged Init.

--- src/tables_io/__init__.py
+++ src/tables_io/__init__.py
@@ -6,63 +6,70 @@ except:  # pylint: disable=bare-except   #pragma: no cover
     version = "unknown"
 
 from .lazy_modules import *
-
 from .table_dict import TableDict
-
 from . import convert as conv
-
 from . import io_utils
-
 from .utils import concat_utils
-
 from .utils import slice_utils
 
 # Exposing the primary functions and interfaces for tables_io
 
-convert_table = conv.conv_table.convert_obj
 
+#
+# Conversion Functions
+#
+
+convert_table = conv.conv_table.convert_obj
 convertObj = conv.conv_table.convert_obj
 """This function is being deprecated, please see `convert_table` instead"""
-
 convert = conv.conv_tabledict.convert
+
+#
+# Write Functions
+#
 
 writeNative = io_utils.write.write_native
 """This function is being deprecated, please see `write_native` instead"""
-
 write_native = io_utils.write.write_native
-
 write = io_utils.write.write
+
+
+#
+# Read Functions
+#
 
 readNative = io_utils.read.read_native
 """This function is being deprecated, please see `read_native` instead"""
-
 read_native = io_utils.read.read_native
-
 read = io_utils.read.read
-
 io_open = io_utils.read.io_open
+check_columns = io_utils.read.check_columns
+
+#
+# Iteration Functions
+#
 
 iterator_native = io_utils.iterator.iterator_native
-
 iteratorNative = io_utils.iterator.iterator_native
 """This function is being deprecated, please see `iterator_native` instead"""
-
 iterator = io_utils.iterator.iterator
 
-concat_table = concat_utils.concat_objs
+#
+# Concatenation Functions
+#
 
+concat_table = concat_utils.concat_objs
 concatObjs = concat_utils.concat_objs
 """This function is being deprecated, please see `concat_table` instead"""
-
 concat = concat_utils.concat
 # TODO: Does this work on only single table or a table_dict?
 
-slice_table = slice_utils.slice_obj
+#
+# Slicing Functions
+#
 
+slice_table = slice_utils.slice_obj
 sliceObj = slice_utils.slice_obj
 """This function is being deprecated, please see `slice_table` instead"""
-
 sliceObjs = slice_utils.slice_objs
 # TODO: Should this even exist?
-
-check_columns = io_utils.read.check_columns
