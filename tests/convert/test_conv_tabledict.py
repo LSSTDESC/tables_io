fec532b0c91d5671c807efe1a94070aa6dc57769
Mubdi Rahman <mubdi@sidratresearch.com>
2025-02-05 17:47:42 -0500

    Started conversion tests

--- tests/convert/test_conv_tabledict.py
+++ tests/convert/test_conv_tabledict.py
@@ -16,3 +16,34 @@ from tables_io.lazy_modules import tables, apTable, apDiffUtils, fits, h5py, pd,
 )
 def test_deps(mod):
     assert check_deps([mod])
+
+
+@pytest.mark.skipif(
+    not check_deps([apTable, pd]), reason="Missing panda or astropy.table"
+)
+@pytest.mark.parametrize(
+    "tType1",
+    [
+        types.AP_TABLE,
+        types.NUMPY_DICT,
+        types.NUMPY_RECARRAY,
+        types.PA_TABLE,
+        types.PD_DATAFRAME,
+    ],
+)
+@pytest.mark.parametrize(
+    "tType2",
+    [
+        types.AP_TABLE,
+        types.NUMPY_DICT,
+        types.NUMPY_RECARRAY,
+        types.PA_TABLE,
+        types.PD_DATAFRAME,
+    ],
+)
+def test_convert_table_dicts(data_tables, tType1, tType2):
+    """Perform type conversion on the cross-product of all types."""
+    odict_1 = convert(data_tables, tType1)
+    odict_2 = convert(odict_1, tType2)
+    tables_r = convert(odict_2, types.AP_TABLE)
+    assert compare_table_dicts(data_tables, tables_r)
