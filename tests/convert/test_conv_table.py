f400d27b0f3a2285ccf2c69caa751fff58834eec
Mubdi Rahman <mubdi@sidratresearch.com>
2025-02-06 16:32:01 -0500

    Migrated conversion utilities

--- tests/convert/test_conv_table.py
+++ tests/convert/test_conv_table.py
@@ -31,8 +31,35 @@ from tables_io.lazy_modules import tables, apTable, apDiffUtils, fits, h5py, pd,
         types.PD_DATAFRAME,
     ],
 )
-def test_convert_table(data_tables, data_table, tType1, tType2):
+def test_convert_table(data_table, tType1, tType2):
     """Perform type conversion for tables."""
     t1 = convert_table(data_table, tType1)
     t2 = convert_table(t1, tType2)
     _ = convert_table(t2, types.AP_TABLE)
+
+
+def test_bad_conversion(data_table):
+    """Tests that the conversion fails as expected when given an incorrect table type."""
+
+    # Testing Case Sensitivity
+    with pytest.raises(TypeError) as e:
+        bad = convert_table(data_table, "astropytable")
+
+    # Testing against out of range key
+    with pytest.raises(KeyError) as e:
+        bad = convert_table(data_table, 500)
+
+    # Testing Wrong Key
+    with pytest.raises(TypeError) as e:
+        bad = convert_table(data_table, "CSV")
+
+
+@pytest.mark.skipif(
+    not check_deps([apTable, pd]), reason="Missing panda or astropy.table"
+)
+def test_conversion_strings(data_table):
+    """Tests that the conversion functions work when given table types as strings."""
+
+    # test convert works with a string
+    tType = types.TABULAR_FORMATS[0]
+    tab = convert_table(data_table, tType)
